8ca7899469e51df0030d1991d3d16525df5135fb
Ircama <amacri@tiscali.it>
2016-10-03 16:04:23 +0200

    website - fix last modification widget

--- public/github.commits.widget.js
+++ public/github.commits.widget.js
@@ -1,6 +1,11 @@
 /*
 
-Modified by ircama
+Modified by ircama, 2016; added the following options:
+- nouser (true/false/unset): if set to true, does not show user
+- nomsg (true/false/unset): if set to true, does not show message
+- abstime (true/false/unset): if set to true, show absolute time
+- noreltime (true/false/unset): if set to true, does not show relative time
+- simple (unset, 1, 2): if set, show a simple messabe without list
 
 https://github.com/alexanderbeletsky/github-commits-widget
 
@@ -37,11 +42,18 @@ THE SOFTWARE.
 
     widget.prototype = (function() {
 
-        function getCommits(user, repo, branch, path, simple, callback) {
+        function getCommits(user, repo, branch, path, simple, element, callback) {
             $.ajax({
                 url: "https://api.github.com/repos/" + user + repo + "/commits?sha=" + branch + (path === undefined ? '' : "&path=" + path),
                 dataType: 'jsonp',
-                success: callback
+                success: callback,
+                error: function(){
+                  if (simple)
+                  {
+                    element.empty();
+                    element.append((simple < 2 ? ' ' : ': ') + '<span class="noprint">check ' + '<a class="github-commit" href="' + 'https://github.com/' + user + repo + '/commits/' + branch + (path === undefined ? '' : '/' + path) + '" target="_blank">here</a></span>');
+                  }
+                }
             });
         }
 
@@ -53,14 +65,20 @@ THE SOFTWARE.
             var callback = widget.callback;
             var element = widget.element;
             var user = widget.options.user;
+            var nouser = widget.options.nouser === undefined ? 0 : nouser = widget.options.nouser;
+            var nomsg = widget.options.nomsg === undefined ? 0 : widget.options.nomsg;
+            var abstime = widget.options.abstime === undefined ? 0 : widget.options.abstime;
+            var noreltime = widget.options.noreltime === undefined ? 0 : widget.options.noreltime;
             var repo = widget.options.repo;
+            if ((repo.length<1) || (repo.substring(0, 1) != '/'))
+              repo = '/' + repo;
             var branch = widget.options.branch;
             var path = widget.options.path;
             var simple = widget.options.simple === undefined ? 0 : widget.options.simple;
             var last = widget.options.last === undefined ? 0 : widget.options.last;
             var limitMessage = widget.options.limitMessageTo === undefined ? 0 : widget.options.limitMessageTo;
 
-            getCommits(user, repo, branch, path, simple, function (data) {
+            getCommits(user, repo, branch, path, simple, element, function (data) {
                 var commits = data.data;
                 var totalCommits = (last < commits.length ? last : commits.length);
 
@@ -73,15 +91,22 @@ THE SOFTWARE.
                     var cur = commits[c];
 
                     if (simple)
-                      var li = $('<a class="github-commit"></a>')
-                        .attr("title", ((simple < 2) ? 'Last commits: ' + commits.length + ', Last is ' : 'Last commit is ') + cur.commit.message)
-                        .attr("href", 'https://github.com/' + user + repo + '/commits/' + branch + (path === undefined ? '' : '/' + path))
-                        .attr("target","_blank")
-                        .appendTo(element);
+                    {
+                    element.append(' ');
+
+                    var li = $('<a class="github-commit">')
+                      .attr("title", ((simple < 2) ? 'Last commits: ' + commits.length + ', ' : 'Last commit: ') + cur.commit.message)
+                      .attr("href", 'https://github.com/' + user + repo + '/commits/' + branch + (path === undefined ? '' : '/' + path))
+                      .attr("target","_blank")
+                      .appendTo(element);
+                    }
                     else
                     {
                       var li = $("<li>");
-  
+                    }
+
+                    if (!nouser)
+                    {
                       var e_user = $('<span class="github-user">');
                       //add github link if possible
                       if (cur.author !== null) {
@@ -91,21 +116,22 @@ THE SOFTWARE.
                       {
                           e_user.append(cur.commit.committer.name);
                       }
+                      li.append(e_user);
                     }
 
-                    if (!simple)
-                      li.append(e_user);
-
                     //add commit message
-                    if (!simple)
+                    if (!nomsg)
                       li.append(message(cur.commit.message, cur.sha));
 
-                    if (simple<2)
+                    if (abstime)
+                    {
                       li.append(timewhen(cur.commit.committer.date));
+                    }
+                    if (!noreltime)
+                      li.append('<span class="noprint">' + (abstime ? ', ' : '') + when(cur.commit.committer.date) + '</span>');
 
-                    li.append(when(cur.commit.committer.date));
-
-                    list.append(li);
+                    if (!simple)
+                      list.append(li);
                 }
 
                 callback(element);
@@ -157,7 +183,7 @@ THE SOFTWARE.
 
                 function timewhen(commitDate) {
                     var localedate = new Date(commitDate).toISOString().slice(0, 10);
-                    return localedate + ', ';
+                    return localedate;
                 }
             });
         }
