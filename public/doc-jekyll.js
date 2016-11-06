b1388173038e33d3020b8a014eaba881e576c38d
Ircama <amacri@tiscali.it>
2016-11-06 21:47:22 +0100

    Revert "	modified:   public/doc-jekyll.js"
    This reverts commit 3636be165d2a8ec1c82eccd95838bc0d128bb4e6.

--- public/doc-jekyll.js
+++ public/doc-jekyll.js
@@ -106,7 +106,19 @@ if ($(window).height() < $('nav[data-toggle="toc"]').height() + $("div.sticky").
 else
   $("div.sticky").css("bottom","unset");
 })
-)
+
+/* Pinch-to-zoom switches off the Table of Content */
+
+$('.container').live("touchstart touchmove touchend", function(e){
+    document.title=e.scale;
+    if (e.scale < 1.0) {
+        // User moved fingers closer together
+        $("div.sticky").css("opacity","1");
+    } else if (e.scale > 1.0) {
+        // User moved fingers further apart
+        $("div.sticky").css("opacity","0");
+    }
+})
 
 /*----------------------------------------------------------------------------------------------*/
 
