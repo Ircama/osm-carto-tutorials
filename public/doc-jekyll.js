7556bcb560ad6285175b4c226ae851293f5c2343
Ircama <amacri@tiscali.it>
2016-11-07 08:53:44 +0100

    modified:   public/doc-jekyll.js

--- public/doc-jekyll.js
+++ public/doc-jekyll.js
@@ -110,7 +110,7 @@ else
 /* Automatically close the the Table of Content on touchscreen moves with two fingers */
 document.addEventListener('touchmove', function(e) {
   if (e.touches.length == 2) { // two fingers
-    var dist = (e.touches[0].screenX-e.originalEvent.touches[1].screenX) * (e.touches[0].screenY-e.originalEvent.touches[1].screenY);
+    var dist = (e.touches[0].pageX-e.touches[1].pageX) * (e.touches[0].pageY-e.touches[1].pageY);
     document.title=dist;
 /* 
     if (dist == 0)
