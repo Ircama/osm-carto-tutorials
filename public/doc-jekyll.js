dd5ddc29d7e7fc9c758e1ef3e5266592040906d1
Ircama <amacri@tiscali.it>
2016-11-07 09:19:11 +0100

    modified:   public/doc-jekyll.js

--- public/doc-jekyll.js
+++ public/doc-jekyll.js
@@ -134,7 +134,7 @@ function checkForOverlap(el1, el2) {
 document.addEventListener('touchmove', function(e) {
   if (e.touches.length == 2) { // two fingers
     var dist = Math.abs((e.touches[0].clientX-e.touches[1].clientX) * (e.touches[0].clientY-e.touches[1].clientY));
-    document.title = checkForOverlap($("div.sticky"), $("div.container"));
+    document.title = checkForOverlap($("div.sticky")[0], $("div.container")[0]);
     if (dist <5000)
       {
         $(".toc-title").css("display","flex");
