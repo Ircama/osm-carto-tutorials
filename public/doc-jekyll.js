1dab721466d929b612cb35ed9e02c49371d61c30
Ircama <amacri@tiscali.it>
2016-11-07 10:26:26 +0100

    modified:   public/doc-jekyll.js

--- public/doc-jekyll.js
+++ public/doc-jekyll.js
@@ -107,6 +107,28 @@ else
   $("div.sticky").css("bottom","unset");
 })
 
+/* Automatically close the Table of Content (with its title) and the sidebar button on touchscreen when overlapping with the document text */
+document.addEventListener('touchend', function() {
+  if (document.querySelector("div.sticky").getBoundingClientRect().left - document.querySelector("div.container").getBoundingClientRect().right > 0)
+    {
+      $(".toc-title").css("opacity","1");
+      $("div.sticky").css("opacity","1");
+    }
+  else
+    {
+      $(".toc-title").css("opacity","0");
+      $("div.sticky").css("opacity","0");
+    }
+  if (document.querySelector("div.container").getBoundingClientRect().left - document.querySelector("label.sidebar-toggle").getBoundingClientRect().right > 0)
+    {
+      $("label.sidebar-toggle").css("opacity","1");
+    }
+  else
+    {
+      $("label.sidebar-toggle").css("opacity","0");
+    }
+}, false);
+
 
 /*----------------------------------------------------------------------------------------------*/
 
@@ -132,28 +154,8 @@ delayCheckFullScreen();
   var sidebar = document.querySelector('#sidebar');
   var checkbox = document.querySelector('#sidebar-checkbox');
   var sticky = document.querySelector('.sticky');
-  var container = document.querySelector("div.container");
 
   document.addEventListener('touchstart', function(e) { /* Automatically close the sidebar when tapping the page content */
-
-  if (sticky.getBoundingClientRect().left - container.getBoundingClientRect().right > 0)
-    {
-      $(".toc-title").css("opacity","1");
-      $("div.sticky").css("opacity","1");
-    }
-  else
-    {
-      $(".toc-title").css("opacity","0");
-      $("div.sticky").css("opacity","0");
-    }
-  if (container.getBoundingClientRect().left - toggle.getBoundingClientRect().right > 0)
-    {
-      $("label.sidebar-toggle").css("opacity","1");
-    }
-  else
-    {
-      $("label.sidebar-toggle").css("opacity","0");
-    }
 
     delayCheckFullScreen();
     var target = e.target;
@@ -164,9 +166,6 @@ delayCheckFullScreen();
 
     checkbox.checked = false;
     sticky.visible = false;
-
-
-
   }, false);
 
   document.addEventListener('click', function(e) { /* Automatically close the sidebar when clicking on the page content */
