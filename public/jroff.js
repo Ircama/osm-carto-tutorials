757655dffbe098c4b1969066649b8ee83bced067
Ircama <amacri@tiscali.it>
2017-06-16 00:54:35 +0200

    modified:   manpage.html 	new file:   public/jroff.css 	modified:   public/jroff.js

--- public/jroff.js
+++ public/jroff.js
@@ -1,3 +1,12 @@
+function urlify(text) {
+    var urlRegex = /((https?:\/\/)|(www\.))[^\s<&]+/g;
+    //var urlRegex = /(https?:\/\/[^\s]+)/g;
+    return text.replace(urlRegex, function(url,b,c) {
+        var url2 = (c == 'www.') ?  'http://' +url : url;
+        return '<a href="' +url2+ '" target="_blank">' + url + '</a>';
+    }) 
+}
+
 /**
  * Jroff 0.0.1 <http://roperzh.github.io/jroff.js>
  * Copyright (c)2015 Roberto Dip <http://roperzh.com>
